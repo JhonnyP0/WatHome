3d2b4e2755830d6021ee9f797bfd90a02e98cc09
Jan <jaspachulski@gmail.com>
2025-02-02 13:13:23 +0100

    template base and css prepared

--- watthome/watthome.py
+++ watthome/watthome.py
@@ -20,7 +20,7 @@ def db_conn():
 
 @app.route("/")
 def example_page():
-    return render_template("examplePage.html")
+    return render_template("base.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5050, debug=True)
